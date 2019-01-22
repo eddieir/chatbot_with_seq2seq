aaf27d8f88c10eaabc7dbf7d089e9f3129a56a95
eddieir <peyman.iravani@gmail.com>
2019-01-22 18:27:31 +0100

    commit until generation creation

--- bot.py
+++ bot.py
@@ -1,5 +1,42 @@
 import os
 import pickle
+import numpy as np
+import pandas as pd
 from six.moves import urllib
 import tflearn
 from tflearn.data_utils import *
+
+path = "shakespeare_input.txt"
+char_idx_file = 'char_idx.pickle'
+
+if not os.path.isfile(path):
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/tflearn/tflearn.github.io/master/resources/shakespeare_input.txt", path)
+
+maxlen = 25
+
+char_idx = None
+if os.path.isfile(char_idx_file):
+  print('Loading previous char_idx')
+  char_idx = pickle.load(open(char_idx_file, 'rb'))
+
+X, Y, char_idx = \
+    textfile_to_semi_redundant_sequences(path, seq_maxlen=maxlen, redun_step=3,
+                                         pre_defined_char_idx=char_idx)
+
+pickle.dump(char_idx, open(char_idx_file,'wb'))
+
+g = tflearn.input_data([None, maxlen, len(char_idx)])
+g = tflearn.lstm(g, 512, return_seq=True)
+g = tflearn.dropout(g, 0.5)
+g = tflearn.lstm(g, 512, return_seq=True)
+g = tflearn.dropout(g, 0.5)
+g = tflearn.lstm(g, 512)
+g = tflearn.dropout(g, 0.5)
+g = tflearn.fully_connected(g, len(char_idx), activation='softmax')
+g = tflearn.regression(g, optimizer='adam', loss='categorical_crossentropy',
+                       learning_rate=0.001)
+
+m = tflearn.SequenceGenerator(g, dictionary=char_idx,
+                              seq_maxlen=maxlen,
+                              clip_gradients=5.0,
+                              checkpoint_path='model_shakespeare')
